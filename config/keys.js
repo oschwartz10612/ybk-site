4bb8741cf3f93973cc1f658def268c2ee7024586
Owen Schwartz <oschwartz10612@gmail.com>
2019-04-08 20:43:46 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  google: {
-    clientID: '1024084421872-0cli2ske2khvbgme00kfo9hrfc5oen5f.apps.googleusercontent.com',
-    clientSecret: 'xZ0qhFzbKdqYqmKJiIm9mjUb'
-  },
-  session: {
-    cookieKey: 'ybkrocks'
-  }
-};
